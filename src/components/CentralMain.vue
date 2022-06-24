e7425ab5eb20f2ab9e6e98eaef1e735ec46c139b
paolopu91 <paolo.pugliese1991@gmail.com>
2022-06-24 16:33:47 +0200

    added main section musicList

--- src/components/CentralMain.vue
+++ src/components/CentralMain.vue
@@ -8,9 +8,11 @@
             <!-- row for my colls of bootstrap -->
            <div class="row row-cols-5">
             <!-- col where i will do v-for -->
-            <div class="col" v-for="(author,i) in musicList" :key="i">
-                ciao
-                <img src="" alt="">
+            <div class="my-col col" v-for="(author,i) in musicList" :key="i">
+                <img :src="author.poster" alt="">
+                <h2>{{author.title}}</h2>
+                <h3>{{author.author}}</h3>
+                <h4>{{author.year}}</h4>
             </div>
            </div>
         </div>
@@ -51,5 +53,28 @@ export default{
 <style scoped>
 .bg-main{
     background-color: #1E2D3B;
+    height: 90vh;
+    padding: 7rem 0;
+}
+.my-col{
+    text-align: center;
+    padding: 1rem;
+}
+img{
+    aspect-ratio: 1/1;
+    height: 180px;
+    max-width: 100%;
+}
+h2{
+    font-size: 20px;
+    text-align: center;
+}
+h3{
+    font-size:15px;
+    text-align: center;
+}
+h4{
+    font-size: 10px;
+    text-align: center;
 }
 </style>
